39dd91b6a8a1fe58724889e4e761d81c31a5d175
locchuong <locchuong123@gmail.com>
2022-07-19 15:14:24 +0700

    building bot

--- selenium/bot/booking/booking.py
+++ selenium/bot/booking/booking.py
@@ -2,14 +2,18 @@ import os
 import booking.constants as const
 from selenium import webdriver
 
-# create a booking class define some methods that you will be reuse in the project, inherited from webdriver.Chrome
-class Booking(webdriver.Chrome):
+'''
+create a booking class define some methods that you will be reuse in the project,
+inherited from webdriver.Chrome
+'''
+
+class Booking(webdriver.Chrome):   
     
     def __init__(self,driver_path= r"C:/Users/PC/Desktop/crawl_data/selenium/driver"):
         self.driver_path = driver_path 
         os.environ['PATH'] += driver_path # when call this class as object, it will be set in os environment variables
         super(Booking,self).__init__() # completely inherit from webdriver.Chrome, you can take the full access of webdriver.Chrome
-    
+
     def land_first_page(self,path = "https://www.booking.com"):
         self.get(const.BASE_URL)
         
